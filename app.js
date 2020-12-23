dbd2cdaec60522d7db8da9b769d65277f4228f61
kristyquach <quachkristy@gmail.com>
2020-12-22 22:07:08 -0800

    Level 4 Salting and Hashing with bcrypt

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -26,7 +25,6 @@ const userSchema = new mongoose.Schema ( {
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,17 +41,20 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password:req.body.password
-  });
 
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
@@ -61,18 +62,19 @@ app.post("/login", function(req,res){
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({email: username}, function(err, foundUser){
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        if (foundUser.password === password){
-          res.render("secrets");
-        }
-      }
-    }
-  });
-});
+  User.findOne({email:username},function(err,foundUser){
+     if(err)
+     console.log(err);
+     else{
+       if(foundUser){
+         if(bcrypt.compareSync(password, foundUser.password))
+         res.render("secrets")
+         else
+         res.send("Invalid Credentials")
+       }
+     }
+   })
+ })
 
 
 app.listen(3000, function() {
